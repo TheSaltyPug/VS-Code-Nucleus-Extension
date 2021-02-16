2cf1d2e5cd5d5b57f94e878fae8921d32bfcdccc
TheSaltyPug <cmdsmithpc@gmail.com>
2021-02-16 14:51:26 -0500

    Added settings variables

--- commands/createNewDatapack.js
+++ commands/createNewDatapack.js
@@ -36,17 +36,20 @@ async function run(uri){
     // grab some settings beforehand so we can do conditions on them
     // for example, settingNamespace is used to set the namespace to author if it is undefined
     let settingNamespace = vscode.workspace.getConfiguration('nucleus')['namespace'];
+    let settingAuthor = vscode.workspace.getConfiguration('nucleus')['author'];
+    let settingMainName = vscode.workspace.getConfiguration('nucleus')['mainName'];
+    let settingLoadName = vscode.workspace.getConfiguration('nucleus')['loadName'];
 
+    // TODO-- don't ask input if a setting is defined
     // input data from the user
-    let author = await getData("Please enter your Minecraft username:", vscode.workspace.getConfiguration('nucleus')['author']); // datapack author. Used in the global advancement
+    let author = await getData("Please enter your Minecraft username:", settingAuthor); // datapack author. Used in the global advancement
     let dname = await getData("Please enter the datapack name:"); //  datapack name. Used in global advancement, mcmeta file, and root folder
-    let namespace = await getData("Please enter the namespace:", ((typeof(settingNamespace) == 'undefined') ? author : vscode.workspace.getConfiguration('nucleus')['namespace'])); // main folder name. namespace:projname/filename
+    let namespace = await getData("Please enter the namespace:", ((typeof(settingNamespace) == 'undefined') ? author : settingNamespace)); // main folder name. namespace:projname/filename
     let projname = await getData("Please enter the project name:"); // project name. namespace:projname/filename
     let item = await getData("Please enter the item id for the datapack advancement:","name_tag");   // datapack item used in the global advancement
     let desc = await getData("Please enter the datapack description:");   // datapack description. Used in global advancment and mcmeta file.
     let mainName = await getData("Please enter the name of the main function:","main"); // name of the function that is run every tick. namespace:projname/mainName
     let loadName = await getData("Please enter the name of the setup function:","setup"); // name of the function that is run on load. namespace:projname/loadName
-
 
     // start generation
 
